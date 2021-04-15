2f75982a766f9836261fe5292120c0e88f62eccf
maks <maksimsozonchik@coherentsolutions.com>
2021-04-15 15:00:26 +0300

    struct prog 11

--- struct_prog/struct_prog_11.py
+++ struct_prog/struct_prog_11.py
@@ -5,10 +5,10 @@ from random import randint
 
 arr = [randint(1, 10) for _ in range(7)]
 print(arr)
-new_arr = list(map(lambda x: x * 2, arr))
+new_arr = list(map(lambda x: x * (- 2), arr))
 print(new_arr)
 
 new_arr_2 = []
 for i in arr:
-    new_arr_2.append(i * 2)
+    new_arr_2.append(i * (- 2))
 print(new_arr_2)
