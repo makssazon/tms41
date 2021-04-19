b20107a6ce3b7b8279f7766a57e95bdf109da35b
maks <maksimsozonchik@coherentsolutions.com>
2021-04-19 18:33:37 +0300

    funcs all

--- func_prog/func_prog_12.py
+++ func_prog/func_prog_12.py
@@ -1,2 +1,29 @@
 # Создать универсальный декоратор, который
 # меняет порядок аргументов в функции на противоположный.
+from functools import wraps
+
+
+def my_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        new_args = tuple([arg for arg in args[::-1]])
+        new_kwargs = {key: value for key, value in list(kwargs.items())[::-1]}
+        func(new_args, new_kwargs)
+
+    return wrapper
+
+
+@my_decorator
+def my_func(*args, **kwargs):
+    for arg in args:
+        print(arg)
+    for kwarg in kwargs:
+        print(kwarg)
+
+
+def main():
+    my_func(1, 2, 3, a=4, b=5, c=6)
+
+
+if __name__ == '__main__':
+    main()
