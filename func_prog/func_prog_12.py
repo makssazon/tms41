212c2dc9fadc349360d80e4d2b9ce75c6be32550
maks <maksimsozonchik@coherentsolutions.com>
2021-04-20 10:51:46 +0300

    func_prog all

--- func_prog/func_prog_12.py
+++ func_prog/func_prog_12.py
@@ -6,19 +6,20 @@ from functools import wraps
 def my_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        new_args = tuple([arg for arg in args[::-1]])
-        new_kwargs = {key: value for key, value in list(kwargs.items())[::-1]}
-        func(new_args, new_kwargs)
+        args = tuple([arg for arg in args[::-1]])
+        kwargs = {key: value for key, value in list(kwargs.items())[::-1]}
+        func(*args, **kwargs)
 
     return wrapper
 
 
+#
 @my_decorator
 def my_func(*args, **kwargs):
     for arg in args:
         print(arg)
-    for kwarg in kwargs:
-        print(kwarg)
+    for kwarg in kwargs.items():
+        print(kwarg, type(kwarg))
 
 
 def main():
