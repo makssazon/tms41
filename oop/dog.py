a0b646a16d244f0cbe70248ba31511e0eae6d6c2
maks <maksimsozonchik@coherentsolutions.com>
2021-04-21 13:13:25 +0300

    oop -8

--- oop/dog.py
+++ oop/dog.py
@@ -3,15 +3,60 @@
 
 class Dog:
     def __init__(self, height, weight, name, age, master, adress='Minsk'):
-        self.height = height
-        self.weight = weight
-        self.name = name
-        self.age = age
+        self.__height = height
+        self.__weight = weight
+        self.__name = name
+        self.__age = age
         self.__master = master
         self.__adress = adress
 
-    def change_name(self, name):
-        self.name = name
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        self.__weight = weight
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @property
+    def master(self):
+        return self.__master
+
+    @master.setter
+    def master(self, master):
+        self.__master = master
+
+    @property
+    def adress(self):
+        return self.__adress
+
+    @adress.setter
+    def adress(self, adress):
+        self.__adress = adress
 
     def jump(self):
         print('Jump!')
@@ -22,15 +67,6 @@ class Dog:
     def bark(self):
         print('Bark!')
 
-    def get_master(self):
-        return self.__master
-
-    def get_adress(self):
-        return self.__adress
-
-    def set_adress(self, adress):
-        self.__adress = adress
-
 
 dog_1 = Dog(0.7, 15, 'Baskerviley', 31, 'nub')
 
@@ -39,11 +75,7 @@ print(dog_1)
 dog_1.jump()
 dog_1.run()
 dog_1.bark()
-print(dog_1.height, dog_1.weight, dog_1.name, dog_1.age)
 
-dog_1.change_name('Sharik')
-print(dog_1.name)
-print(dog_1.get_master())
-print(dog_1.get_adress())
-dog_1.set_adress('Drybin')
-print(dog_1.get_adress())
+print(dog_1.height, dog_1.adress, dog_1.master)
+dog_1.height = 1
+print(dog_1.height)
