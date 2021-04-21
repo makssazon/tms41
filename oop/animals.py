da2ba4771703b5b90e44446096c1e355140e8c63
maks <maksimsozonchik@coherentsolutions.com>
2021-04-21 17:32:16 +0300

    oop animals 12

--- oop/animals.py
+++ oop/animals.py
@@ -4,11 +4,8 @@
 # run, jump, birthday(увеличивает age на 1), sleep.
 # Класс Parrot имеет дополнительный метод fly. Cat - meow, Dog - bark.
 
-class Dog:
-    def __init__(self, name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
+
+class Pet:
 
     def run(self):
         print('Run!')
@@ -21,50 +18,57 @@ class Dog:
 
     def sleep(self):
         print('Sleep!')
+
+
+class Dog(Pet):
+    def __init__(self, name, age, master):
+        self.name = name
+        self.age = age
+        self.master = master
 
     def bark(self):
         print('Bark!')
 
 
-class Cat:
+class Cat(Pet):
     def __init__(self, name, age, master):
         self.name = name
         self.age = age
         self.master = master
-
-    def run(self):
-        print('Run!')
-
-    def jump(self):
-        print('jump!')
-
-    def birthday(self):
-        self.age += 1
-
-    def sleep(self):
-        print('Sleep!')
 
     def meow(self):
         print('Meow!')
 
 
-class Parrot:
+class Parrot(Pet):
     def __init__(self, name, age, master):
         self.name = name
         self.age = age
         self.master = master
 
-    def run(self):
-        print('Run!')
-
-    def jump(self):
-        print('jump!')
-
-    def birthday(self):
-        self.age += 1
-
-    def sleep(self):
-        print('Sleep!')
-
     def fly(self):
         print('Fly!')
+
+
+dog = Dog('rex', 10, 'igar')
+dog.jump()
+dog.birthday()
+print(dog.age)
+dog.sleep()
+dog.bark()
+dog.run()
+
+cat = Cat('murka', 5, 'Valera')
+cat.run()
+cat.sleep()
+cat.jump()
+cat.birthday()
+print(cat.age)
+cat.meow()
+
+parrot = Parrot('gosha', 3, 'Alla')
+parrot.run()
+parrot.jump()
+parrot.sleep()
+parrot.birthday()
+parrot.fly()
