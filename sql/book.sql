db5c21a8977eae150a2e0e1f4988a96d003982b7
maks <maksimsozonchik@coherentsolutions.com>
2021-04-29 13:13:32 +0300

    sql 7 select with where

--- sql/book.sql
+++ sql/book.sql
@@ -17,11 +17,15 @@ CREATE TABLE Book (
 
 ALTER TABLE book ADD COLUMN release_year int;
 
-INSERT INTO Book (title, pages, author, price, release_year)
-values ('master', 380, 'Bulgakov', 25, 1965),
-('harry', 600, 'Rolling', 35, 2001),
-('skot', 300, 'Oryel', 30, 1956),
-('norvegian wood', 200, 'murakami', 40, 1995),
-('dva kapitana', 500, 'kaverin', 20, 1955)
+--INSERT INTO Book (title, pages, author, price, release_year)
+--values ('master', 380, 'Bulgakov', 25, 1965),
+--('harry', 600, 'Rolling', 35, 2001),
+--('skot', 300, 'Oryel', 30, 1956),
+--('norvegian wood', 200, 'murakami', 40, 1995),
+--('dva kapitana', 500, 'kaverin', 20, 1955)
 
+select release_year, title, price FROM Book;
+
+select title, release_year,price FROM Book
+WHERE release_year = 2001
 
