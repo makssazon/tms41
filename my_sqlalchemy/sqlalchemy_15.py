8b43a3e8e0077c29e1f99591a97a2c2068b35deb
maks <maksimsozonchik@coherentsolutions.com>
2021-05-06 12:30:33 +0300

    sqlalchemy all

--- my_sqlalchemy/sqlalchemy_15.py
+++ my_sqlalchemy/sqlalchemy_15.py
@@ -1,7 +1,15 @@
-from school.models import session, Group, Student
+from school.models import session, Group, Student, Diary
 
-result = session.query(Group, Student).\
+result = session.query(Group, Student). \
     join(Student, Group.id == Student.group_id). \
     filter(Student.firstname == 'max')
 for group, st in result:
     print(group.name, st.firstname)
+
+print('-' * 15)
+
+result2 = session.query(Student, Diary). \
+    join(Diary, Student.id == Diary.student_id). \
+    filter(Diary.avg_score > 6)
+for st, d in result2:
+    print(st.firstname, d.avg_score)
