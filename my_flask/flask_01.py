fae4fd4e6c1ddaa27345a87411d220264d91aa52
maks <maksimsozonchik@coherentsolutions.com>
2021-05-17 14:10:23 +0300

    flask 1

---
+++ my_flask/flask_01.py
@@ -0,0 +1,22 @@
+# Создать сайт. При запросе на домашнюю страницу отображается текущая дата.
+
+from datetime import datetime
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'hello world'
+
+
+def time_now():
+    return f'time is {datetime.now()}'
+
+
+app.add_url_rule('/', 'time_now', time_now)
+
+if __name__ == '__main__':
+    app.run()
