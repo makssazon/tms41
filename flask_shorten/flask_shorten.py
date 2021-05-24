0c9965191fe6beb9431196c75e1ffebbd7dc153d
maks <maksimsozonchik@coherentsolutions.com>
2021-05-24 12:41:52 +0300

    flask shorten tests

--- flask_shorten/flask_shorten.py
+++ flask_shorten/flask_shorten.py
@@ -32,19 +32,19 @@ def read_links():
         return render_template('index.html', data=data,
                                next_id=next_id, url=request.url)
     else:
-        link1 = AllLinks.query.filter_by(origin_link=request.form['origin']).\
+        link1 = AllLinks.query.filter_by(origin_link=request.form['origin']). \
             first()
-        link2 = AllLinks.query.filter_by(short_link=request.form['short']).\
+        link2 = AllLinks.query.filter_by(short_link=request.form['short']). \
             first()
         if link1:
             return render_template('success.html', result=1,
-                                   link=link1, url=request.url)
+                                   link=link1, url=request.url), 400
         elif link2:
             return render_template('success.html', result=2,
-                                   link=link2, url=request.url)
+                                   link=link2, url=request.url), 400
         else:
             if not request.form['short']:
-                return render_template('success.html', result=5)
+                return render_template('success.html', result=5), 400
             else:
                 link = AllLinks(origin_link=request.form['origin'],
                                 short_link=request.form['short'],
@@ -62,9 +62,9 @@ def go_link(end=None):
     if link:
         link.counter += 1
         db.session.commit()
-        return redirect(link.origin_link)
+        return redirect(link.origin_link), 303
     else:
-        return render_template('success.html', result=4, url=request.url)
+        return render_template('success.html', result=4, url=request.url), 404
 
 
 if __name__ == '__main__':
