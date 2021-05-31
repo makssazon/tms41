0a03c99dd426c7dc72072d5f676ce86dea409b02
maks <maksimsozonchik@coherentsolutions.com>
2021-05-31 18:43:46 +0300

    flask shorten add flask-login, wtf

--- flask_shorten/flask_shorten.py
+++ flask_shorten/flask_shorten.py
@@ -1,9 +1,13 @@
 import datetime
 
-from flask import request, render_template, redirect
+from flask import request, render_template, redirect, url_for, flash
+from flask_login import login_user, login_required, current_user, logout_user
+from sqlalchemy import or_
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_shorten.app import app
-from flask_shorten.models import AllLinks
+from flask_shorten.forms import LoginForm, RegisterForm
+from flask_shorten.models import AllLinks, User
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -13,7 +17,7 @@ def read_links():
         next_id = data_last.id + 1 if data_last else 1
         data = AllLinks.query.all()
         return render_template('index.html', data=data,
-                               next_id=next_id, url=request.url)
+                               next_id=next_id, url=request.url, user=current_user)
     else:
         link1 = AllLinks.query.filter_by(origin_link=request.form['origin']). \
             first()
@@ -47,3 +51,51 @@ def go_link(end=None):
         return redirect(link.origin_link), 303
     else:
         return render_template('success.html', result=4, url=request.url), 404
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if request.method == 'GET':
+        return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        email = form.email.data
+        psw = form.psw.data
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.hash_password, psw):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('profile'))
+
+    return render_template('login.html', form=form), 400
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    if request.method == 'GET':
+        return render_template('register.html', form=form)
+    if form.validate_on_submit():
+        name = form.username.data
+        email = form.email.data
+        psw = generate_password_hash(form.psw.data)
+        if User.query.filter(or_(User.name == name, User.email == email)).first():
+            flash('user with email or name is used', category='error')
+            return render_template('register.html', form=form), 400
+        user = User(name=name, email=email, hash_password=psw)
+        user.save()
+        flash(f'user {name} was created', category='success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form), 400
+
+
+@app.route('/profile', methods=['POST', 'GET'])
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('read_links'))
--- flask_shorten/models.py
+++ flask_shorten/models.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
+
 from flask_shorten.app import db, app
 
 
@@ -15,6 +20,28 @@ class AllLinks(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    hash_password = db.Column(db.String, nullable=False, unique=True)
+    created_on = db.Column(db.DateTime, default=datetime.now)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def check_password(self, password):
+        return check_password_hash(self.hash_password, password)
+
+    def __repr__(self):
+        return f'{self.id} {self.name}'
 
 
 db.init_app(app)
