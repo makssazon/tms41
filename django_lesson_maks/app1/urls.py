c6ed836c635561c08afb87ff238e644f9a436da6
maks <maksimsozonchik@coherentsolutions.com>
2021-06-09 19:14:06 +0300

    django 06

--- django_lesson_maks/app1/urls.py
+++ django_lesson_maks/app1/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from app1.views import my_time, two_pow, my_word, add_user, form
+from app1.views import my_time, two_pow, my_word, add_user, form, full_form
 
 urlpatterns = [
     path('', my_time, name='time'),
     path('two_pow/<int:number>', two_pow, name='two_pow'),
     path('my_word/<str:word>', my_word, name='my_word'),
     path('add_user', add_user, name='add_user'),
-    path('form', form, name='form')
+    path('form', form, name='form'),
+    path('full_form', full_form, name='full_form')
 
 ]
--- django_lesson_maks/app1/views.py
+++ django_lesson_maks/app1/views.py
@@ -52,3 +52,13 @@ def form(request):
         template = loader.get_template('django_05.html')
         response = template.render({}, request)
         return HttpResponse(response)
+
+
+def full_form(request):
+    if request.method == 'POST':
+        context = {'user': {'firstname': request.POST.get('firstname'),
+                            'lastname': request.POST.get('lastname'),
+                            'age': request.POST.get('age')}}
+        return render(request, 'django_06_display.html', context)
+    if request.method == 'GET':
+        return render(request, 'django_06_form.html')
