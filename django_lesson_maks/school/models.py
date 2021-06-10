6735fd25cf88bbf00bd134b1416ff40766c32e3c
maks <maksimsozonchik@coherentsolutions.com>
2021-06-10 20:22:53 +0300

    django 15

--- django_lesson_maks/school/models.py
+++ django_lesson_maks/school/models.py
@@ -3,3 +3,15 @@ from django.db import models
 
 class Group(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    group = models.ForeignKey(Group, null=False, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.firstname
